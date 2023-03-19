ce1c67ed92c4e757a317895c2c1aa3fc48c3252d
Lachuuuu <108222128+Lachuuuu@users.noreply.github.com>
2023-03-19 17:32:42 +0100

    added movie Page and updated TopBar

---
+++ src/components/MovieDescriptionHeader.js
@@ -0,0 +1,8 @@
+import {styled} from "@mui/system";
+
+const MovieDescriptionHeader = styled("span")({
+    fontWeight: "bold",
+    fontSize: "large"
+})
+
+export default MovieDescriptionHeader;
--- src/components/topBar/TopBar.jsx
+++ src/components/topBar/TopBar.jsx
@@ -1,15 +1,38 @@
+import * as React from 'react';
+import {useEffect, useState} from 'react';
 import Styles from "./TopBar.module.css"
-import {AppBar, FormControl, IconButton, InputAdornment, TextField, Toolbar} from "@mui/material";
-import PersonIcon from '@mui/icons-material/Person';
-import {useState} from "react";
+import {
+    AppBar,
+    Button,
+    FormControl,
+    IconButton,
+    InputAdornment,
+    Menu,
+    MenuItem,
+    TextField,
+    Toolbar
+} from "@mui/material";
 import {Form} from "react-router-dom";
 import SearchIcon from '@mui/icons-material/Search';
-import AccountLink from "../AccountLink";
 import LogoLink from "../LogoLink";
+import {changeLocation, getCookie} from "../../api/Api";
+import getApiUrl from "../../api/ApiUrl";
+import PersonIcon from "@mui/icons-material/Person";
+import AccountLink from "../AccountLink";
 
 function TopBar() {
 
-    const [searchValue, setSearchValue] = useState("")
+
+    const [menuOpen, setMenuOpen] = useState(false)
+    const [anchorEl, setAnchorEl] = React.useState(null)
+    const [searchValue, setSearchValue] = useState(null)
+    const [userName, setUserName] = useState("")
+
+    useEffect(() => {
+        let cookie = getCookie("firstName")
+        if (cookie === null) setUserName(null)
+        else setUserName(cookie.valueOf())
+    }, [])
 
     return (
         <AppBar position="sticky">
@@ -39,14 +62,66 @@ function TopBar() {
                         </FormControl>
                     </Form>
                 </div>
+
                 <div className={Styles.accountBox}>
-                    <PersonIcon className={Styles.personIcon}/>
-                    <AccountLink href="/login">Logowanie</AccountLink>
-                    <AccountLink href="/register">Rejestracja</AccountLink>
+                    <PersonIcon onClick={event => openMenu(event)} className={Styles.personIcon}/>
+                    {userName !== null && <div>
+                        <div onClick={event => openMenu(event)}>{userName}</div>
+                        <Menu
+                            anchorEl={anchorEl}
+                            id="accountMenu"
+                            open={menuOpen}
+                            onClick={closeMenu}
+                            onClose={closeMenu}
+                            disableScrollLock={true}
+                        >
+                            <MenuItem>
+                                <div>Historia</div>
+                            </MenuItem>
+                            <MenuItem>
+                                <div id="user/settings"
+                                     onClick={event => changeLocation(event.target.id)}>Ustawienia
+                                </div>
+                            </MenuItem>
+                            <MenuItem onClick={logout}>
+                                <Button variant="outlined">Wyloguj</Button>
+                            </MenuItem>
+                        </Menu>
+                    </div>
+                    }
+                    {userName === null &&
+                        <AccountLink href="/login">Logowanie</AccountLink>
+                    }
+                    {userName === null &&
+                        <AccountLink href="/register">Rejestracja</AccountLink>
+                    }
                 </div>
             </Toolbar>
         </AppBar>
     );
+
+    async function logout() {
+        await fetch(getApiUrl() + "auth/logout", {
+                method: "GET",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                credentials: "include"
+            }
+        )
+        changeLocation("")
+        document.location.reload()
+    }
+
+    function openMenu(event) {
+        if (userName !== "" || userName != null) setMenuOpen(true)
+        setAnchorEl(event.currentTarget)
+    }
+
+    function closeMenu() {
+        setMenuOpen(false)
+    }
 }
 
 export default TopBar;
