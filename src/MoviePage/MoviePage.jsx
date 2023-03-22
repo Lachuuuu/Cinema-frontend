2db132313abc0c912e89b3ecac21aac2c73cf520
Lachuuuu <108222128+Lachuuuu@users.noreply.github.com>
2023-03-22 01:14:48 +0100

    v0.1 you can choose seats and showing for movie but you cant complet reservation yet

--- src/MoviePage/MoviePage.jsx
+++ src/MoviePage/MoviePage.jsx
@@ -7,17 +7,39 @@ import "react-image-gallery/styles/css/image-gallery.css";
 import {useParams} from "react-router-dom";
 import GlassBox from "../components/GlassBox";
 import MovieDescriptionHeader from "../components/MovieDescriptionHeader";
+import {Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField} from "@mui/material";
+import classNames from "classnames";
 
 function MoviePage() {
 
     const [responseStatus, setResponseStatus] = useState(-1)
     const [movie, setMovie] = useState(null)
+    const [open, setOpen] = useState(false)
+    const [normalTicket, setNormalTicket] = useState(0)
+    const [discountTicket, setDiscountTicket] = useState(0)
+    const [dialogScreenId, setDialogScreenId] = useState(0)
+    const [showing, setShowing] = useState(null)
+    const choosenSeats = []
+    let seatId = -1;
 
     const {movieId} = useParams()
 
     useEffect(() => {
         downloadMovie();
     }, [])
+
+    useEffect(() => {
+        console.log(movie)
+    }, [movie])
+
+    useEffect(() => {
+        console.log(showing)
+    }, [showing])
+
+    useEffect(() => {
+        console.log(choosenSeats)
+    }, [choosenSeats])
+
 
     return (<>
             <div className={Styles.main}>
@@ -42,12 +64,119 @@ function MoviePage() {
                         <div>
                             <MovieDescriptionHeader>Age:</MovieDescriptionHeader> {movie.minAge}+
                         </div>
-
+                        <div>
+                            {movie.showings.map(it => <Button onClick={event => handleOpenDialog(event.target.value)}
+                                                              value={it.id}>{it.showingStartTime}</Button>)}
+                        </div>
                     </GlassBox>
                 }
             </div>
+
+            <Dialog open={open} onClose={handleCloseDialog}>
+                {dialogScreenId === 0 && <>
+                    <DialogTitle>Wybierz bilety</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            normal - 15 PLN
+                            student/kid - 10 PLN
+                        </DialogContentText>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="normal"
+                            label="normal ticket"
+                            type="number"
+                            variant="standard"
+                            InputProps={{inputProps: {min: 0}}}
+                            onChange={event => setNormalTicket(event.target.value)}
+                        />
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="discount"
+                            label="student/kid"
+                            type="number"
+                            variant="standard"
+                            InputProps={{inputProps: {min: 0}}}
+                            onChange={event => setDiscountTicket(event.target.value)}
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleCloseDialog}>Cancel</Button>
+                        <Button onClick={handleNextScreen}>Next</Button>
+                    </DialogActions>
+                </>
+                }
+                {dialogScreenId === 1 && <>
+                    <DialogTitle>Wybierz miejsca</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            zielone - wolne
+                            czerwone - zajete
+                        </DialogContentText>
+                        <div className={Styles.seatsBox}>
+                            {showing.hall.seatsMap.split('|').map(row =>
+                                <div className={Styles.rowBox}>
+                                    {row.split('').map(it => chooseSeat(it))}
+                                </div>
+                            )}
+                        </div>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleCloseDialog}>Cancel</Button>
+                        <Button onClick={handleNextScreen}>Next</Button>
+                    </DialogActions>
+                </>
+                }
+            </Dialog>
         </>
     );
+
+    function handleCloseDialog() {
+        setOpen(false)
+        setDialogScreenId(0)
+        seatId = -1;
+    }
+
+    function handleOpenDialog(id) {
+        setOpen(true)
+        setShowing(movie.showings.find(element => element.id == id))
+    }
+
+    function handleNextScreen() {
+        if (dialogScreenId != 0 || (dialogScreenId == 0 && (normalTicket > 0 || discountTicket > 0)))
+            if (dialogScreenId === 1) setDialogScreenId(0)
+            else setDialogScreenId(dialogScreenId + 1)
+    }
+
+    function chooseSeat(it) {
+        seatId++
+        switch (it) {
+            case '0':
+                return <div className={classNames(Styles.seatFlor)}></div>
+                break;
+            case '1':
+                return <div className={classNames(Styles.seatFree, Styles.seat)}
+                            onClick={event => takeSeat(event.target.getAttribute("seatid"), event.target)}
+                            seatid={seatId}></div>
+                break;
+            case '2':
+                return <div className={classNames(Styles.seatTaken)}></div>
+                break;
+            default:
+                return <br/>
+                break;
+        }
+    }
+
+    function takeSeat(id, target) {
+        const indexOfElement = choosenSeats.indexOf(id)
+        if (choosenSeats.length < parseInt(normalTicket) + parseInt(discountTicket) || indexOfElement != -1) {
+            target.classList.toggle(Styles.seatChoosen)
+            if (indexOfElement == -1) choosenSeats.push(id)
+            else choosenSeats.splice(indexOfElement, 1)
+        }
+    }
 
     async function downloadMovie() {
 
